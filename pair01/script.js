99270ddc354a19b45fdcd2506f1bc94c2cf343e3
Remaust <121612118+Remaust@users.noreply.github.com>
2026-09-07 11:12:09 +0300

    Add files via upload

---
+++ pair01/script.js
@@ -0,0 +1,56 @@
+// var a;
+// a = 10;
+// console.log(a);
+//
+// var b = 20;
+// var c = 12, d = 12;
+// let aNew = '32';
+
+// a = 13;
+// let a;
+// console.log(a);
+
+// const a = 4;
+
+// let a = 15, b = 26;
+// console.log(a + b);
+// console.log(b - a);
+// console.log(a * b);
+// console.log(a ** b);
+// console.log(a / b);
+// console.log(a % b);
+//
+// a += 1;
+// a++;
+// a--;
+// ++a;
+// let a = 2, b;
+// // b = a++;
+// b = ++a;
+// console.log(b);
+
+// let a = '4';
+// let b = 2;
+// console.log(a - b);
+// console.log(a + b);
+//
+// a = Number(a);
+// console.log(a + b);
+
+// let name = "Ivan";
+// let age = 18;
+//
+// console.log(`Привіт ${name}! Тобі ${age} років.`);
+//
+// console.info("hello world");
+// console.log("hello world");
+// console.warn("hello world");
+// console.error("hello world");
+
+// alert("hello");
+// let name = prompt("what is your name?");
+// alert(name);
+
+let a = Number(prompt('number1?'));
+let b = Number(prompt('number2?'));
+console.log(a + b);
